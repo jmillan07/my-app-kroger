62d7d7ffa878b3b3a37a4f9e285f79f3c70a82ab
Javier Millan <56491113+jmillan07@users.noreply.github.com>
2022-09-25 18:54:36 -0500

    Update Employee.jsx

--- src/Employee.jsx
+++ src/Employee.jsx
@@ -1,63 +1,58 @@
-import { useState } from 'react'
-import { useNavigate,Outlet } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
 
+export function Employee() {
+  let navigate = useNavigate();
+  const [usernameInput, setUsernameInput] = useState("");
+  const [passInput, setPassInput] = useState("");
 
-export function Employee(){
+  const handleInput = (e) => setUsernameInput(e.target.value);
 
-    let navigate = useNavigate();
-    const [usernameInput, setUsernameInput] = useState("");
-    const [passInput, setPassInput] = useState("");
+  const handleInputPass = (e) => setPassInput(e.target.value);
 
-
-    const handleInput = (e) => setUsernameInput(e.target.value);
-
-    const handleInputPass = (e) => setPassInput(e.target.value);
-
-    const login = (e) => {
-        e.preventDefault();
-        const item = { ...localStorage };
-        const datoArray = Object.values(item)
-        for(let i =0; i<datoArray.length;i++){
-            const dataObject =JSON.parse(datoArray[i])
-            if (
-                usernameInput === dataObject.usuario &&
-                passInput === dataObject.clave
-              ) {
-                navigate(`/Employee/${dataObject.key}`);
-                break
-              }
-        }
+  const login = (e) => {
+    e.preventDefault();
+    const item = { ...localStorage };
+    const datoArray = Object.values(item);
+    for (let i = 0; i < datoArray.length; i++) {
+      const dataObject = JSON.parse(datoArray[i]);
+      if (
+        usernameInput === dataObject.usuario &&
+        passInput === dataObject.clave
+      ) {
+        navigate(`/Employee/${dataObject.key}`);
+        break;
       }
+    }
+  };
 
-    return (
-        <>
-        <h1>Empleador</h1>
-        <div className='container'>
+  return (
+    <>
+      <h1>Empleador</h1>
+      <div className="container">
+        <input
+          autoComplete="off"
+          onChange={handleInput}
+          placeholder="Usuario"
+          name="username"
+          type="text"
+          value={usernameInput}
+          className="inputs"
+        />
 
-            <input
-            autoComplete="off"
-            onChange={handleInput}
-            placeholder="Usuario"
-            name="username"
-            type="text"
-            value={usernameInput}
-            className="inputs"
-          
-            />
+        <input
+          placeholder="Password"
+          name="password"
+          type="password"
+          onChange={handleInputPass}
+          value={passInput}
+          className="inputs"
+        />
 
-            <input
-            placeholder="Password"
-            name="password"
-            type="password"
-            onChange={handleInputPass}
-            value={passInput}
-            className="inputs"
-            />
- 
-            <button onClick={login}>login</button>
+        <button onClick={login}>login</button>
 
-            <Outlet/>
-            </div>
-        </>
-    )
+        <Outlet />
+      </div>
+    </>
+  );
 }
